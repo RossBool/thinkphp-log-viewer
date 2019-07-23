a3dcae1f9f9f55d6e1c4b6a443072130336b4fd0
zhoujunren <junrenzhou@rastar.com>
2019-07-23 15:27:27 +0800

    feat(functions.php)：添加助手函数

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,14 @@
 <?php
+/**
+ * @deprecated 助手函数
+ * @author RossBool
+ * @copyright   2019-07-23
+ */
+
+if (!function_exists('getIp')) {
+    function getIp()
+    {
+        return $_SERVER['SERVER_ADDR'];
+
+    }
+}
